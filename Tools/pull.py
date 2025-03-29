06ca6584719f55a862d98765faf84801596ad6d8
Nettking <m@nettking.no>
2025-03-29 09:31:41 +0100

    Update pull.py

--- Tools/pull.py
+++ Tools/pull.py
@@ -1,6 +1,7 @@
 from llm import LLM
 
 if __name__ == "__main__":
-    MODEL = input("Modell name to pull: ")
     llm = LLM()
+    llm.list_available_models()
+    MODEL = input("Modell name to pull: ")
     llm.pull_model(MODEL)
